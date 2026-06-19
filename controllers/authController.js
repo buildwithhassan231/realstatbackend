56970ea9892e2685717879996076f802646aed6e
Hadssan <hassankhan847754@gmail.com>
2026-06-19 18:00:55 +0500

    feat(auth): Add JWT authentication and login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import { UserRegisterSchema } from "../models/register.js";
-
+import jwt from "jsonwebtoken"
 export const register = async (req, res) => {
   try {
     const { name, email, password, phoneNumber } = req.body;
@@ -45,6 +45,76 @@ export const register = async (req, res) => {
         name: user.name,
         email: user.email,
         phoneNumber: user.phoneNumber,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and Password are required",
+      });
+    }
+
+    // Check user
+    const user = await UserRegisterSchema.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        role: user.role,
       },
     });
   } catch (error) {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectdb } from "./config/db.js";
-import { register } from "./controllers/authController.js";
+import { Login, register } from "./controllers/authController.js";
 
 const app = express();
 
@@ -11,6 +11,7 @@ connectdb();
 app.use(express.json());
 
 app.post("/api/register" , register);
+app.post("/api/login" , Login);
 
 app.listen(process.env.PORT, ()=>{
     console.log("server is running on port ", process.env.PORT )
